68ef443fbc73c9025e3264affe859d82873ac1f4
Frank Basham <frankabasham@gmail.com>
2022-09-21 09:08:55 -0400

    alter sidebar

--- components/Sidenav.js
+++ components/Sidenav.js
@@ -11,7 +11,7 @@ const Sidenav = () => {
     >
       <button
         aria-label="toggle side menu"
-        className="w-8 h-8 absolute left-[265px] space-y-1 bg-gray-800 p-2 cursor-pointer rounded hover:bg-gray-900"
+        className="w-8 h-8 absolute left-[265px] space-y-1 bg-gray-900 p-2 cursor-pointer rounded hover:bg-gray-800"
         onClick={(e) => setOpen(!open)}
       >
         <div
@@ -26,9 +26,47 @@ const Sidenav = () => {
           }`}
         ></div>
       </button>
-      <div className="p-2 flex flex-col fixed w-64 min-h-full bg-gray-600 text-white text-xl tracking-wide">
+      <div className="p-2 flex flex-col gap-4 fixed w-64 min-h-full bg-[#373737] text-white text-xl tracking-wide">
         <Link href="/home">
           <a className="hover:bg-periwinkle px-2">Home</a>
+        </Link>
+        <label tabIndex={0} labelFor="about-us" className="flex flex-col px-2">
+          <div className="flex items-center">
+            About us <span className="text-[9px] ml-2">&#9660;</span>
+          </div>
+          <input
+            id="about-us"
+            type="checkbox"
+            className="items-center peer hidden"
+          ></input>
+          <Link href="">
+            <a className="peer-checked:hidden hover:bg-periwinkle px-5">
+              Our Coaches
+            </a>
+          </Link>
+          <Link href="">
+            <a className="peer-checked:hidden hover:bg-periwinkle px-5">
+              Service Catalogue
+            </a>
+          </Link>
+        </label>
+        <Link href="">
+          <a className="hover:bg-periwinkle px-2">Dojo Assesment</a>
+        </Link>
+        <Link href="">
+          <a className="hover:bg-periwinkle px-2">Dojo Engagement</a>
+        </Link>
+        <Link href="">
+          <a className="hover:bg-periwinkle px-2">Events</a>
+        </Link>
+        <Link href="">
+          <a className="hover:bg-periwinkle px-2">Tools &amp; Resources</a>
+        </Link>
+        <Link href="">
+          <a className="hover:bg-periwinkle px-2">FAQ</a>
+        </Link>
+        <Link href="">
+          <a className="hover:bg-periwinkle px-2">Contact us</a>
         </Link>
       </div>
     </div>
