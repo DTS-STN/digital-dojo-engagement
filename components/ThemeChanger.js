e55d040d0c363ae0f1d2d9e019342739ae367dbe
Frank Basham <frankabasham@gmail.com>
2022-09-28 08:07:39 -0400

    change license / add mounting check

--- components/ThemeChanger.js
+++ components/ThemeChanger.js
@@ -1,8 +1,17 @@
 import { useTheme } from 'next-themes'
 import { BsMoon, BsSun } from 'react-icons/bs'
+import { useState, useEffect } from 'react'
 
 export default function ThemeChanger() {
   const { theme, setTheme } = useTheme()
+  const [isMounted, setIsMounted] = useState(false)
+
+  // needed for rehydration mismatch between server/client
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if (!isMounted) return null
 
   return (
     <button
