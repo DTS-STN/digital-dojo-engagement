9005e8adbcd94b235cafb81dc78f4ff9b3e0171a
Adam <adam.andrews2797@gmail.com>
2022-10-03 16:23:06 -0400

    updated CSS for coaches

--- components/Coach.js
+++ components/Coach.js
@@ -6,15 +6,20 @@ function Coach({ src, name, title, blurb }) {
     <div className="flex flex-col items-center">
       <div className="rounded-full border-2 p-1 h-32 w-32">
         {src ? (
-          <img src={src} className="rounded-full bg-cover" />
+          <Image
+            width={500}
+            height={500}
+            src={src}
+            className="rounded-full bg-cover"
+          />
         ) : (
           <BsFillPersonFill className="w-28 h-28" />
         )}
       </div>
-      <h2 className="font-bold text-periwinkle text-lg text-center">
+      <p className="font-bold text-periwinkle text-lg text-center">
         {name || 'Name'}
-      </h2>
-      <p className="text-md text-center">{title || 'Title'}</p>
+      </p>
+      <p className="text-md pb-2 text-center">{title || 'Title'}</p>
       <p className="text-sm text-center">{blurb || 'Blurb'}</p>
     </div>
   )
