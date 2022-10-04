a4a92091cdee5fd3fc9d0595e1c2a82e34b9a7d4
Adam <adam.andrews2797@gmail.com>
2022-10-04 15:41:45 -0400

    Added presentation tag to the dotted line so screen readers ignore it

--- components/DottedLine.js
+++ components/DottedLine.js
@@ -2,7 +2,10 @@ import PropTypes from 'prop-types'
 
 export default function DottedLine({ numDots }) {
   return (
-    <ul className="flex flex-initial flex-row justify-center w-full my-5">
+    <ul
+      role="presentation"
+      className="flex flex-initial flex-row justify-center w-full my-5"
+    >
       {[...Array(numDots)].map((_, i) => (
         <li
           className="justify-center bg-[#b7c1e1] aspect-square w-[17px] rounded-full mx-3.5"
