a81e591fbeeadb4b91c668e1518af5fbfc041f7c
Frank Basham <frankabasham@gmail.com>
2022-10-07 10:46:38 -0400

    finish translations

---
+++ locales/belt-assessment/belt-assessment/fr.js
@@ -0,0 +1,113 @@
+export default {
+  title: 'Système de Ceinture',
+  p1: "L'auto-évaluation du système Digital Dojo Belt est conçue pour aider votre équipe à identifier les connaissances actuelles de votre équipe dans les domaines Agile, DevOps et Design Thinking. Cela aidera l'équipe du Dojo à travailler avec vous pour améliorer vos compétences tout au long d'un engagement.",
+  h2: 'Comment Évaluer',
+  p2: 'Ci-dessous, nous avons classé les',
+  s1: '4 domaines Dojo numérique',
+  p3: 'et leur',
+  s2: 'les principes clés.',
+  p4: 'Pour chaque catégorie, sélectionnez la couleur de ceinture correspondante qui convient le mieux décrit la maturité actuelle de votre équipe.',
+  p5: "Vos résultats d'évaluation de la ceinture nous aideront à créer une session personnalisée où nous pouvons identifier ensemble les domaines d'intérêt, en ajoutant le plus de valeur à votre projet.",
+  getResults: 'Obtenir des Résultats',
+  select:
+    "Sélectionnez l'une des ceintures ci-dessous qui décrit le mieux où se trouve votre équipe actuellement à :",
+  whiteBelt: 'Ceinture Blanche:',
+  yellowBelt: 'Ceinture Jaune:',
+  greenBelt: 'Ceinture Verte:',
+  blackBelt: 'Ceinture Noire:',
+  na: "N'est Pas Applicable.",
+  agile: 'Agile',
+  agileDevops: 'Agile/DevOps',
+  devops: 'DevOps',
+  designThinking: 'La Pensée de Conception',
+  continuousPlanning: 'Planification Continue',
+  continuousPlanningLong:
+    "Nous créons des plans courts et flexibles qui peuvent être adaptés à l'évolution les circonstances et les priorités changeantes, s'adaptant aux imprévus retards. Le processus de planification continue implique le suivi des objectifs, des mesures et des jalons pour les priorités existantes ; identifier, développer et faire progresser de nouvelles priorités stratégiques ; et assurer une culture de l'auto-évaluation, de l'innovation, de l'agilité et de l'adaptabilité.",
+  continuousPlanningWhite:
+    "Notre équipe ne fait pas avoir un arriéré de travail clair et nous n'utilisons pas de suivi du travail système.",
+  continuousPlanningYellow:
+    'Nous avons des réunions régulières pour planifier, revoir, affiner le travail et les tâches sont suivis dans un outil.',
+  continuousPlanningGreen:
+    'Nous avons un arriéré qui est priorisé et maintenu avec des objectifs fixés, et nous travaillons dans cycles courts.',
+  continuousPlanningBlack:
+    "Nous avons une feuille de route pour notre service/produit, et continuellement mettre à jour notre plan basé sur Retour d'information reçu.",
+  continuousCollaboration: 'Collaboration Continue',
+  continuousCollaborationLong: 'Placeholder',
+  continuousCollaborationWhite:
+    "Le travail est assigné et nous ne savons pas toujours qui travaille sur quoi, ou s'ils ont du mal.",
+  continuousCollaborationYellow:
+    'Nous avons quotidiennement stand-ups et notre travail est visible pour les autres équipes et parties prenantes.',
+  continuousCollaborationGreen:
+    'Nous avons documenté nos façons de travailler en équipe et convenu de notre utilisation de outils de collaboration.',
+  continuousCollaborationBlack:
+    'Nous nous auto-organisons grâce à la gestion des tâches et partageons nos connaissances dans des communautés de pratiques.',
+  continuousImprovement: 'Amélioration Continue',
+  continuousImprovementLong: 'Placeholder',
+  continuousImprovementWhite:
+    "Notre équipe ne fait pas avoir un mécanisme pour discuter des points douloureux ou suggérer des façons d'améliorer comment nous travaillons.",
+  continuousImprovementYellow:
+    'Notre équipe se réunit régulièrement des rétrospectives pour échanger sur notre façon de travailler (personnes, processus, outils).',
+  continuousImprovementGreen:
+    'Nous prenons des mesures articles de nos rétrospectives et ajoutez-les à notre sprint arriéré.',
+  continuousImprovementBlack:
+    'Notre équipe inspecte, adapte et automatise sans relâche nos processus pour offrir plus rapidement de la valeur aux utilisateurs.',
+  continuousQuality: 'Qualité Continue',
+  continuousQualityLong: 'Placeholder',
+  continuousQualityWhite:
+    'Nous effectuons rarement revues de code, et la plupart de nos tests sont manuels.',
+  continuousQualityYellow:
+    'Certains tests sont automatique; nous collaborons avec des experts en sécurité informatique et en accessibilité pour revoir notre code.',
+  continuousQualityGreen:
+    'Nous codons et maintenir les données de test et mesurer la couverture du code.',
+  continuousQualityBlack:
+    'Nous continuons revoir notre suite de tests pour mieux trouver les défauts et notre pipeline comprend des revues de code.',
+  continuousIntegration: 'Intégration Continue',
+  continuousIntegrationLong: 'Placeholder',
+  continuousIntegrationWhite:
+    'Nous avons notre code base on prem dans le contrôle de code source.',
+  continuousIntegrationYellow:
+    "Notre code s'engage entraîner une construction automatisée du logiciel avec une version stratégie de contrôle.",
+  continuousIntegrationGreen:
+    "Notre code s'engage aboutissent à une série de tests automatisés et les commits sont liés à un tâche.",
+  continuousIntegrationBlack:
+    'Le logiciel est dans un état déployable tout au long de son cycle de vie, les dépendances sont code stable et cassant rarement.',
+  continuousDelivery: 'Livraison Continue',
+  continuousDeliveryLong: 'Placeholder',
+  continuousDeliveryWhite:
+    "Uniquement désigné les membres de l'équipe préparent des versions, qui impliquent de nombreuses étapes manuelles.",
+  continuousDeliveryYellow:
+    "Les builds/déploiements sont reproductibles, vérifiables et peuvent être effectués par n'importe quel membre de l'équipe.",
+  continuousDeliveryGreen:
+    'Les builds/déploiements sont déclenchés automatiquement et automatisés les tests font partie de notre pipeline.',
+  continuousDeliveryBlack:
+    'Tout le monde peut déployer notre produit à la production ou aux utilisateurs finaux à tout moment sur demande.',
+  continuousOperations: 'Opérations Continues',
+  continuousOperationsLong: 'Placeholder',
+  continuousOperationsWhite:
+    'Créer de nouveaux environnements est un processus manuel, avec un accès limité à journaux et métriques de production.',
+  continuousOperationsYellow:
+    'Il y a procédures en place pour nous informer des incidents avec accès à journaux de fabrication.',
+  continuousOperationsGreen:
+    "Nous pouvons créer et gérez les instances avec des tableaux de bord pour visualiser l'état du serveur.",
+  continuousOperationsBlack:
+    'Il y a processus automatisés en place pour répondre rapidement aux incidents et nous pouvons dynamiquement augmenter ou diminuer les ressources.',
+  continuousSecurity: 'Sécurité Continue',
+  continuousSecurityLong: 'Placeholder',
+  continuousSecurityWhite: 'Nous avons limité tests de sécurité.',
+  continuousSecurityYellow:
+    "Nous codons avec la sécurité à l'esprit et les environnements résident dans un magasin sécurisé.",
+  continuousSecurityGreen:
+    'Sécurité les exigences sont incluses dans le processus de test automatisé pour le développement et fabrication.',
+  continuousSecurityBlack:
+    'Notre code est scanné pour les problèmes de sécurité et des processus automatisés sont en place pour répondre rapidement aux problèmes de sécurité.',
+  continuousUserFeedback: 'Commentaires Continus des Utilisateurs',
+  continuousUserFeedbackLong: 'Placeholder',
+  continuousUserFeedbackWhite:
+    'Nous définissons notre exigences initiales et sont définies comme des règles métier uniquement.',
+  continuousUserFeedbackYellow:
+    'Notre équipe utilise commentaires des utilisateurs finaux pour établir et valider les exigences de notre service.',
+  continuousUserFeedbackGreen:
+    "Nous avons défini personas et documents requis du point de vue de l'utilisateur (ex : Histoires d'utilisateurs); et recueillir activement les commentaires des clients.",
+  continuousUserFeedbackBlack:
+    'Nous avons un processus ou des outils en place pour intégrer les commentaires des utilisateurs, y compris utilisateurs anonymes dans la conception du service.',
+}
