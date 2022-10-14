0d3ea73593d27c6adb13f929e2de1b9c656f780a
Frank Basham <frankabasham@gmail.com>
2022-10-14 13:31:01 -0400

    add page test

---
+++ __tests__/pages/engagement.test.js
@@ -0,0 +1,41 @@
+import { render, screen, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Engagement from '../../pages/engagement'
+import { getStaticProps } from '../../pages/engagement'
+
+describe('engagement page', () => {
+  it('renders page', () => {
+    render(<Engagement />)
+    expect(screen.getByText('Start a Team Engagement')).toBeInTheDocument()
+  })
+
+  it('writes to form', () => {
+    render(<Engagement />)
+    const fname = screen.getByLabelText('First Name *')
+    fireEvent.change(fname, { target: { value: 'test first name' } })
+    expect(fname.value).toBe('test first name')
+  })
+
+  it('Test getStaticProps', async () => {
+    const props = await getStaticProps({ locale: 'en' })
+    expect(props).toEqual({
+      props: {
+        locale: 'en',
+        meta: {
+          data_en: {
+            title: 'Digital Dojo - Team Engagement',
+            desc: 'English',
+            author: '',
+            keywords: '',
+          },
+          data_fr: {
+            title: "Dojo Numérique - Mobilisation d'équipe",
+            desc: 'Français',
+            author: '',
+            keywords: '',
+          },
+        },
+      },
+    })
+  })
+})
