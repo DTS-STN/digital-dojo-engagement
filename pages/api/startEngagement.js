dd78b8b4417b75c1c2301e0e01335f50e1c930a0
Frank Basham <frankabasham@gmail.com>
2022-10-17 12:13:10 -0400

    attempt to resolve zap xss warning, possibly a false positive

--- pages/api/startEngagement.js
+++ pages/api/startEngagement.js
@@ -1,4 +1,5 @@
 import nodemailer from 'nodemailer'
+import sanitizeHTML from 'sanitize-html'
 
 export default async function (req, res) {
   const transporter = nodemailer.createTransport({
@@ -17,15 +18,23 @@ export default async function (req, res) {
       to: process.env.NODEMAILER_EMAIL,
       subject: 'Start Dojo Team Engagement',
       html: `<dl>
-        <dt><b>First Name:</b></dt><dt>${req.body.first_name}</dt>
-        <dt><b>Last Name:</b></dt><dt>${req.body.last_name}</dt>
-        <dt><b>Email:</b></dt><dt>${req.body.email}</dt>
-        <dt><b>Team Name:</b></dt><dt>${req.body.team_name}</dt>
-        <dt><b>Work Location:</b></dt><dt>${req.body.work_location}</dt>
-        <dt><b>Dates in mind?:</b></dt><dt>${req.body.dates}</dt>
-        <dt><b>Subject to train:</b></dt><dt>${req.body.practice}</dt>
-        <dt><b>What to know about them:</b></dt><dt>${req.body.what_else}</dt>
-        <dt><b>How did they find us?:</b></dt><dt>${req.body.how_find}</dt>
+        <dt><b>First Name:</b></dt><dt>${sanitizeHTML(req.body.first_name)}</dt>
+        <dt><b>Last Name:</b></dt><dt>${sanitizeHTML(req.body.last_name)}</dt>
+        <dt><b>Email:</b></dt><dt>${sanitizeHTML(req.body.email)}</dt>
+        <dt><b>Team Name:</b></dt><dt>${sanitizeHTML(req.body.team_name)}</dt>
+        <dt><b>Work Location:</b></dt><dt>${sanitizeHTML(
+          req.body.work_location
+        )}</dt>
+        <dt><b>Dates in mind?:</b></dt><dt>${sanitizeHTML(req.body.dates)}</dt>
+        <dt><b>Subject to train:</b></dt><dt>${sanitizeHTML(
+          req.body.practice
+        )}</dt>
+        <dt><b>What to know about them:</b></dt><dt>${sanitizeHTML(
+          req.body.what_else
+        )}</dt>
+        <dt><b>How did they find us?:</b></dt><dt>${sanitizeHTML(
+          req.body.how_find
+        )}</dt>
       </dl>`,
     })
     res.status(200).json({ message: 'success' })
