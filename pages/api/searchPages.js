40a171542d9e01ecc4991c08c162b8c0b0a043c3
Frank Basham <frankabasham@gmail.com>
2022-11-01 12:28:01 -0400

    api change

--- pages/api/searchPages.js
+++ pages/api/searchPages.js
@@ -2,5 +2,5 @@ import fs from 'fs'
 
 export default async function (req, res) {
   const data = await fs.promises.readFile('./postbuild/pageData.json', 'utf-8')
-  return res.json(JSON.parse(data) || 'something went wrong')
+  res.json(JSON.parse(data))
 }
