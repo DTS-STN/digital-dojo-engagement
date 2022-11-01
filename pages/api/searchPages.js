03f20c0565234f9781effbb9164921e1c5e72a51
Frank Basham <frankabasham@gmail.com>
2022-11-01 12:35:21 -0400

    more debugging...

--- pages/api/searchPages.js
+++ pages/api/searchPages.js
@@ -2,5 +2,6 @@ import fs from 'fs'
 
 export default async function (req, res) {
   const data = await fs.promises.readFile('./postbuild/pageData.json', 'utf-8')
-  res.json(JSON.parse(data))
+  //   res.json(JSON.parse(data))
+  res.json('this should return something....')
 }
