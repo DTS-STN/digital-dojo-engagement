42253569392c816f18c5ad2bc1d4465bef325f53
Frank Basham <frankabasham@gmail.com>
2022-11-01 14:03:10 -0400

    debug path

--- pages/api/searchPages.js
+++ pages/api/searchPages.js
@@ -5,7 +5,7 @@ import process from 'process'
 export default async function (req, res) {
   try {
     const data = await fs.promises.readFile(
-      path.join(__dirname, '../../../../postbuild/pageData.json'),
+      path.join(process.cwd(), '/postbuild/pageData.json'),
       'utf-8'
     )
     res.json(JSON.parse(data))
