67ca8a4fbd090242c63ad4cfe6211b9ecc7c7201
Frank Basham <frankabasham@gmail.com>
2022-11-09 12:46:54 -0500

    resolve unit test issues

--- jest.config.js
+++ jest.config.js
@@ -29,11 +29,13 @@ module.exports = {
   transform: {
     /* Use babel-jest to transpile tests with the next/babel preset
       https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+    '^.+\\.(js|jsx|ts|tsx)$': [
+      'babel-jest',
+      { presets: ['next/babel', '@babel/preset-env'] },
+    ],
   },
   transformIgnorePatterns: [
     '/node_modules/',
     '^.+\\.module\\.(css|sass|scss)$',
   ],
-  presets: ['@babel/preset-env'],
 }
