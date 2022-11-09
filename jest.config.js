1bed795ee4dc6030e1a22efcf27e3d0846f3167d
Frank Basham <frankabasham@gmail.com>
2022-11-09 12:49:07 -0500

    revert changes

--- jest.config.js
+++ jest.config.js
@@ -29,10 +29,7 @@ module.exports = {
   transform: {
     /* Use babel-jest to transpile tests with the next/babel preset
       https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
-    '^.+\\.(js|jsx|ts|tsx)$': [
-      'babel-jest',
-      { presets: ['next/babel', '@babel/preset-env'] },
-    ],
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
   },
   transformIgnorePatterns: [
     '/node_modules/',
