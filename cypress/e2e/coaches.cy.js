97c1bdf098d51335324852d7fea829be2fe2e44c
Frank Basham <frankabasham@gmail.com>
2022-12-14 08:52:22 -0500

    add e2e tests

---
+++ cypress/e2e/coaches.cy.js
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+
+describe('coaches page loads', () => {
+    beforeEach(() => {
+      cy.visit('/about/coaches')
+      cy.injectAxe();
+    })
+  
+    it('displays the coaches page', () => {
+      cy.url().should("contains", "/about/coaches");
+    })
+
+    it('should locate h1',()=>{
+        cy.get('h1').should('contain.text','Our Team')
+    })  
+  
+    it('Home page has no detectable a11y violations on load', () => {
+      cy.checkA11y()
+    })
+  })
+  
